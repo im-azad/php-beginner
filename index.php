ff900085f94a7432a8188df1760412c4a95e70b6
Abul Kalam Azad <me.azad99@gmail.com>
2025-05-16 09:11:55 +0600

    run hello world

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>HTML Tutorial</title>
+</head>
+<body>
+
+<h1><?php 
+$my_name = "John Doe";
+
+/* This is a comment */
+echo "Hello $my_name!"; 
+
+
+?></h1>
+<p>This is a paragraph.</p>
+ 
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,7 @@
+<?php
+
+// Constant variable`
+$firsName = "John";
+$lastName = "Doe";
+
+echo $firsName . " " . $lastName;
