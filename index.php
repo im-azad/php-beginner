d0336785af356353189d31d608a8777c39d076d5
Abul Kalam Azad <me.azad99@gmail.com>
2025-05-17 10:00:30 +0600

    conditional rendering

--- index.php
+++ index.php
@@ -5,9 +5,38 @@
     <title>HTML Tutorial</title>
 </head>
 
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+        display: grid;         
+        place-items: center;
+        background-color: #ffefef;
+    }
+ 
+</style>
+
 <body>
 
+<!-- Conditional Rendering -->
+
+    <?php 
+        $book_name = "Quran";
+        $is_read = false;
+        if($is_read) {
+            $message = "You have read $book_name, Good!";
+        } else {
+            $message = "You have not read $book_name, Bad!!";
+        }
+    ?>
     <h1>
+        <?php echo $message; ?>
+
+    </h1>
+    <p> <?= $message ?> </p>
+
+    <!-- Variable and print -->
+    <h2>
         <?php
             $my_name = "John Doe";
 
@@ -15,7 +44,8 @@
             echo "Hello $my_name!";
 
 
-        ?></h1>
+        ?>
+    </h2>
     <p>This is a paragraph.</p>
     <p>
         <?php
