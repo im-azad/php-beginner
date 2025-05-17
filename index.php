05d3eddd381bc004c594a41718d37a6d0d70d192
Abul Kalam Azad <me.azad99@gmail.com>
2025-05-17 09:37:13 +0600

    variable add

--- index.php
+++ index.php
@@ -1,19 +1,39 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>HTML Tutorial</title>
+    <title>HTML Tutorial</title>
 </head>
+
 <body>
 
-<h1><?php 
-$my_name = "John Doe";
+    <h1>
+        <?php
+            $my_name = "John Doe";
 
-/* This is a comment */
-echo "Hello $my_name!"; 
+            /* This is a comment */
+            echo "Hello $my_name!";
 
 
-?></h1>
-<p>This is a paragraph.</p>
- 
+        ?></h1>
+    <p>This is a paragraph.</p>
+    <p>
+        <?php
+            echo "Hi azad!!" ;
+        ?>
+    </p>
+    <h4>
+        <?php 
+            print "What are you doing now";
+        ?>
+    </h4>
+    <p>
+        <?php
+            echo "this is fun";
+        ?>
+
+    </p>
+
 </body>
+
 </html>
