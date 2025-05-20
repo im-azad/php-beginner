a87d7203c7c4835de630e5588727575ac712c849
Abul Kalam Azad <me.azad99@gmail.com>
2025-05-20 08:28:36 +0600

    gradient partials add

--- views/services.view.php
+++ views/services.view.php
@@ -1,6 +1,7 @@
 <?php require('partials/head.php'); ?>
 <?php require('partials/nav.php'); ?>
 <div class="relative isolate px-6 pt-14 lg:px-8">
+    <?php require('partials/gradients.php'); ?>
     <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div class="text-center">
             <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Services Page</h1>
