a58097c77906abeb86e1c04b5500936a8ac6f203
Abul Kalam Azad <me.azad99@gmail.com>
2025-05-27 23:10:25 +0600

    router controler added

--- index.php
+++ index.php
@@ -2,23 +2,4 @@
 
 require 'functions.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI']) ['path'];
-$routes = [
- '/' => 'controllers/index.php',
- '/about' => 'controllers/about.php',
- '/contact' => 'controllers/contact.php',
- '/services' => 'controllers/services.php',  
-];
-
-function abort() {
-    http_response_code(404);
-    require 'views/404.view.php';
-    die();
-}
-
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-} else {
-    abort();
-}
-
+require 'router.php';
